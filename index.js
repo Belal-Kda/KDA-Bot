eba0da6d432caa832b359a093c0196ad23aa962e
Belal-Kda <zbxbox525@gmail.com>
2024-06-20 17:42:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["KDA"];
+const statusMessages = ["KDA Community","صلي على رسول الله ❤️"];
 
 
 let currentIndex = 0;
@@ -98,7 +98,7 @@ client.once('ready', () => {
 
   setInterval(() => {
     updateStatusAndSendMessages();
-  }, 10000);
+  }, 5000);
 });
 
 login();
